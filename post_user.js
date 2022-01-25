c681db1648974459e9ecd5e64329a73bf802b3a6
Joceline Marquez <andrea.marquezvivaldo@gmail.com>
2022-01-24 23:54:25 -0600

    Cambio post y delete

--- post_user.js
+++ post_user.js
@@ -1,16 +1,15 @@
 const axios = require('axios');
-const { get } = require('http');
 
 async function makePostRequest() {
 
-    params = {
-        id: 6,
-        first_name: 'Fred',
-        last_name: 'Blair',
-        email: 'freddyb34@gmail.com'
+    params = { //parametros a guardar
+        id: 7, 
+        first_name: 'Joceline',
+        last_name: 'Marquez',
+        email: 'brolyk123@gmail.com'
       }
 
-      //let res = await;
+      let res = await axios.post('http://localhost:3000/users',params); //se utiliza el metodo .post() para realizar un post request en el Json users y se agregan los atributos de params
 
     console.log(res.data);
 }
